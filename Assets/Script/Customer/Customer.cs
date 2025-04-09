df83f8a4089786070f48f70b50ed8aaa5a6a20cb
haru0sameee <it232183@trident.ac.jp>
2025-04-09 10:46:27 +0900

    Revert "コミット整理"
    This reverts commit c2e053fd55e2f3f1a698286b1ebf150d1f246892.

--- Assets/Script/Customer/Customer.cs
+++ Assets/Script/Customer/Customer.cs
@@ -6,9 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class Customer : MonoBehaviour
 {
-    [SerializeField] GameObject finishMask;
+    //[SerializeField] GameObject finishMask;
+    // [SerializeField] int customer = 1;
+    // 残り人数
+    //[SerializeField] TextMeshProUGUI rest;
+    // 終了したときに表示するキャンバス
+    //[SerializeField] GameObject finishMask;
     private SceneChange s;
     private Basket basket;
+    // ステート
     
     void Start()
     {
@@ -18,26 +24,50 @@ public class Customer : MonoBehaviour
 
     void Update()
     {
-        //時間切れになったら
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            finishMask.gameObject.SetActive(true);
+        ////時間切れになったら
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    finishMask.gameObject.SetActive(true);
 
-        }
+        //}
         //シーン管理用のスクリプト欲しいかもめも
-        if (Input.GetMouseButtonDown(0) && finishMask.gameObject.active==true)
-            SceneManager.LoadScene("ResultScene");
+        //if (Input.GetMouseButtonDown(0) && finishMask.gameObject.active==true)
+        //    SceneManager.LoadScene("ResultScene");
+        //if (customer <= 0)
+        //{
+        //    finishMask.gameObject.SetActive(true);
 
+        //    //シーン管理用のスクリプト欲しいかもめも
+        //    if(Input.GetKeyDown(KeyCode.Space)) 
+        //    SceneManager.LoadScene("ResultScene");
+        //}
         // rest.text="残り" + customer.ToString()+"人";
         //rest.text=customer.ToString();
+
+
+        // 移動したら○秒待機
+
+        // 画面外へ移動後、削除
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Basket") && basket != null && basket.GetIsFinish()==true)
         {
-
+            //customer--;
             Debug.Log("完了");
-        }
+			// 新しくお客さんをスポーンさせる
+			CustomerSpawner.Instance.SpawnCustomer();
+		}
     }
+
+    // スポーンした後の動き
+    private void SpownMovement()
+    {
+
+    }
+
+	// フルーツをもらった後の動き
+
 }
